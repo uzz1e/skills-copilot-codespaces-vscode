a5b27bc723a6e30e72c988b6d1e4ab1874454097
uzz1e <55525001+uzz1e@users.noreply.github.com>
2023-07-28 07:01:00 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsmember() {
+    var x = document.getElementById("skills");
+    var y = document.getElementById("member");
+    var z = document.getElementById("skillsmember");
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
+}
